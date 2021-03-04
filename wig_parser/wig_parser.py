05bf90b898fbe04c17da675a94a094c96ddcf4d5
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-03-03 17:43:35 -0700

    updated wig parser to be able to handle more complex wigs

--- wig_parser/wig_parser.py
+++ wig_parser/wig_parser.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python3
 
 def parse_wig(path):
-    my_parsed_wig = {}
+    my_parsed_wig_full = {}
+    my_parsed_wig = []
+    my_parsed_wig_full["sections"] = my_parsed_wig
     line_number = 0
-    my_parsed_wig["entries"] = []
     with open(path, "r") as inconn:
         for line in inconn:
             line = line.rstrip('\n')
             split_line = line.split()
             if line_number == 0:
-                my_parsed_wig["header"] = line
+                my_parsed_wig_full["header"] = line
             elif len(line) >= len("variable") and line[:8] == "variable":
-                my_parsed_wig["encoding"] = split_line[0]
+                my_parsed_wig.append({})
+                my_parsed_wig[-1]["entries"] = []
+                my_parsed_wig[-1]["encoding"] = split_line[0]
                 chromosome_name = split_line[1].split('=')[1]
                 chromosome_number_string = chromosome_name.replace("chr", "")
                 if chromosome_number_string == "M":
                     chromosome_number = 17
                 else:
                     chromosome_number = int(chromosome_number_string)
-                my_parsed_wig["file_path"] = path
-                my_parsed_wig["chromosome_name"] = chromosome_name
-                my_parsed_wig["chromosome_number_string"] = chromosome_number_string
-                my_parsed_wig["chromosome_number"] = chromosome_number
-                my_parsed_wig["span"] = int(split_line[2].split('=')[1])
+                my_parsed_wig[-1]["file_path"] = path
+                my_parsed_wig[-1]["chromosome_name"] = chromosome_name
+                my_parsed_wig[-1]["chromosome_number_string"] = chromosome_number_string
+                my_parsed_wig[-1]["chromosome_number"] = chromosome_number
+                my_parsed_wig[-1]["span"] = int(split_line[2].split('=')[1])
             else:
                 if len(split_line) >= 2:
                     entry = {}
                     entry["position"] = int(split_line[0])
                     entry["value"] = float(split_line[1])
-                    my_parsed_wig["entries"].append(entry)
+                    my_parsed_wig[-1]["entries"].append(entry)
             line_number = line_number+1
-    return(my_parsed_wig)
+    return(my_parsed_wig_full)
 
 def read_and_filter_wigs(file_containing_paths):
     paths = []
@@ -44,6 +47,12 @@ def read_and_filter_wigs(file_containing_paths):
             wigs.append(parsed_wig)
     return(wigs)
 
+def has_entries(wig):
+    for section in wig["sections"]:
+        if len(section["entries"]) > 0:
+            return True
+    return False
+
 def main():
     file_list = []
     for line in sys.stdin:
@@ -52,7 +61,7 @@ def main():
     wig_list = []
     for path in file_list:
         parsed_wig = parse_wig(path)
-        if len(parsed_wig["entries"]) > 0:
+        if has_entries(parsed_wig):
             wig_list.append(parsed_wig)
             # print(path)
     print(wig_list)
